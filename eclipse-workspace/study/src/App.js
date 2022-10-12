29e515b333803501c698447df4d499474e560747
Redston Kim <gusduswk610@gmail.com>
2022-10-12 14:55:08 +0900

    리엑트 기타 study

---
+++ eclipse-workspace/study/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./componet/Header";
+import Main from "./componet/Main";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>s
+        <Main/>
+    </div>
+  );
+}
+
+export default App;
